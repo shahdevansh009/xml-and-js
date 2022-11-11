76beb2c87198c87e3a76d0ef595260a1028173b4
shahdevansh009 <shahdevansh009@gmail.com>
2022-11-11 12:58:11 -0500

    Complete Module 8 Activity

--- module-08/sync.js
+++ module-08/sync.js
@@ -1,22 +1,36 @@
-function inc(a) {
-  return a + 1;
+const wait = (ms = 1000) => new Promise((resolve) => setTimeout(resolve,ms));
+
+const inc = (a) => 
+  new Promise((resolve)=>{
+    wait().then(()=> resolve(a+1));
+  })
+
+const sum = (a,b) => {
+  return new Promise((resolve) => {
+    wait().then(() => resolve(a+b));
+  })
 }
 
-const sum = function (a, b) {
-  return a + b;
-};
+const max = (a,b) => {
+  return new Promise((resolve) => {
+    wait().then(() => resolve(a > b ? a : b));
+  })
+}
 
-const max = (a, b) => (a > b ? a : b);
 
-const avg = (a, b) => {
-  const s = sum(a, b);
-  return s / 2;
-};
+const avg = (a,b) => {
+  return new Promise((resolve) => {
+    wait().then(() => sum(a,b))
+    .then((s)=> resolve(s/2))
+  });
+}
 
 const obj = {
   name: "Marcus Aurelius",
   split(sep = " ") {
-    return this.name.split(sep);
+    return new Promise((resolve)=> {
+      wait().then(()=> resolve(this.name.split(sep)) );
+    })
   },
 };
 
@@ -26,19 +40,28 @@ class Person {
   }
 
   static of(name) {
-    return new Person(name);
+    return new Promise((resolve) => 
+    wait().then(()=> resolve (new Person(name))));
   }
 
   split(sep = " ") {
-    return this.name.split(sep);
+    return new Promise((resolve)=> {
+      wait().then(() => resolve(this.name.split(sep)));
+    })
   }
 }
 
-const person = Person.of("Marcus Aurelius");
 
-console.log("inc(5) =", inc(5));
-console.log("sum(1, 3) =", sum(1, 3));
-console.log("max(8, 6) =", max(8, 6));
-console.log("avg(8, 6) =", avg(8, 6));
-console.log("obj.split() =", obj.split());
-console.log("person.split() =", person.split());
+
+const main = async()=>{
+  const person = await Person.of("Marcus Aurelius");
+  console.log(person);
+  console.log("inc(5) =", await inc(5));
+  console.log("sum(1, 3) =", await sum(1, 3));
+  console.log("max(8, 6) =", await max(8, 6));
+  console.log("avg(8, 6) =", await avg(8, 6));
+  console.log("obj.split() =", await obj.split());
+  console.log("person.split() =", await person.split());
+};
+
+main();
